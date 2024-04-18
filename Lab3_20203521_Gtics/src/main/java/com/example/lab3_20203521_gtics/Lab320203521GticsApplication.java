5d344db73dcea801b8c084cfd8b8d40d6158f21d
Julio Cesar <a20203521@pucp.edu.pe>
2024-04-17 19:24:12 -0500

    commit 1 master

---
+++ Lab3_20203521_Gtics/src/main/java/com/example/lab3_20203521_gtics/Lab320203521GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203521_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320203521GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320203521GticsApplication.class, args);
+    }
+
+}
---
+++ Lab3_20203521_Gtics/src/test/java/com/example/lab3_20203521_gtics/Lab320203521GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203521_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320203521GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
